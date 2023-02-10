dea30fb275066571c825a1c9299125db5fa1b2cc
berstova <zh.berstova@yandex.ru>
2023-02-10 12:59:03 +0300

    Added C++ file.

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iosrteam>
+#include <sring>;
+
+int main() {
+	std::string first_name, last_name;
+	std::cout << "Enter your first name: ";
+	std::cin >> first_name;
+	std::cout << "Now enter your last name: ";
+	std::cin >> last_name;
+	std::cout << "Hello, " <<first_ name << " " <<last_name << "!";
+}
